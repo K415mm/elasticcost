05df0956334ea4a59525dfdd0d5fa711eea44756
ElasticCost Developer <developer@elasticcost.local>
2026-07-15 18:41:22 +0100

    temp: fix autoloader in render.php

--- render.php
+++ render.php
@@ -1,8 +1,7 @@
 <?php
 use Illuminate\Contracts\Console\Kernel;
 
-require __DIR__.'/bootstrap/app.php';
-
+require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
